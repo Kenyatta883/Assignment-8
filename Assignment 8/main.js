0ff941b40450e368cd4bf66294760f7a367fc264
Kenyatta883 <54584987+Kenyatta883@users.noreply.github.com>
2019-11-14 00:08:46 -0600

    Add files via upload

---
+++ Assignment 8/main.js
@@ -0,0 +1,39 @@
+for(let i = 0; i < 1024; i++){
+    document.querySelector(".app").innerHTML += '<div></div>';
+}
+
+document.body.addEventListener('click', event =>{
+    event.target.style.backgroundColor = '#fff';
+    event.target.style.border = '1px solid #000';
+})
+
+
+document.addEventListener('keydown', event => {
+    if(event.key ==='K'){
+        document.querySelector(".app").style.backgroundColor ='#30f';
+    }
+     if(event.key ==='e'){
+        document.querySelector(".app").style.backgroundColor ='#60f';
+     }
+
+     if(event.key ==='n'){
+         document.querySelector(".app").style.backgroundColor ='#90f';
+     }
+
+    if(event.key ==='y'){
+         document.querySelector(".app").style.backgroundColor ='#a0f';
+    }
+
+    if(event.key ==='a'){
+         document.querySelector(".app").style.backgroundColor ='#c0f';  
+    }      
+})
+document.querySelector('.clear').addEventListener('click', event =>{
+    window.location = window.location;
+})
+document.body.addEventListener('click', event => {
+    if(event.ctrlKey){
+        event.target.innerText = document.querySelector('#text').Value;
+        event.target.style.backgroundColor='rgb(0,'+ blue + ',0)';
+    }
+})
